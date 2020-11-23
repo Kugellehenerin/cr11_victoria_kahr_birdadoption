fe0475133788db83a33980a8ed24ec815fde114c
Victoria Kahr <victoria.kahr@icloud.com>
2020-11-23 21:50:38 +0100

    sql

--- cr11_victoria_kahr_birdadoption.sql
+++ cr11_victoria_kahr_birdadoption.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2020 at 04:22 PM
+-- Generation Time: Nov 23, 2020 at 09:49 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -29,29 +29,30 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `birds` (
   `id` int(11) NOT NULL,
+  `bird_img` varchar(250) NOT NULL,
   `bird_breed` varchar(30) DEFAULT NULL,
   `bird_name` varchar(30) DEFAULT NULL,
   `bird_age` varchar(30) DEFAULT NULL,
-  `bird_description` varchar(30) DEFAULT NULL
+  `bird_description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `birds`
 --
 
-INSERT INTO `birds` (`id`, `bird_breed`, `bird_name`, `bird_age`, `bird_description`) VALUES
-(1, 'African Grey', 'Leyla', '2', 'Beschreibung'),
-(2, 'African Grey', 'Rio', '1', 'Beschreibung'),
-(3, 'African Grey', 'Leroy', '3', 'Beschreibung'),
-(4, 'African Grey', 'Hanni', '4', 'Beschreibung'),
-(5, 'African Grey', 'Rachel', '5', 'Beschreibung'),
-(6, 'African Grey', 'Levi', '10', 'Beschreibung'),
-(7, 'African Grey', 'Lini', '22', 'Beschreibung'),
-(8, 'African Grey', 'Didi', '38', 'Beschreibung'),
-(9, 'African Grey', 'Henry', '41', 'Beschreibung'),
-(10, 'African Grey', 'Lynn', '55', 'Beschreibung'),
-(11, 'African Grey', 'Mietzi', '60', 'Beschreibung'),
-(12, 'African Grey', 'Manolo', '57', 'Beschreibung');
+INSERT INTO `birds` (`id`, `bird_img`, `bird_breed`, `bird_name`, `bird_age`, `bird_description`) VALUES
+(1, 'img/parrot_1.jpg', 'African Grey', 'Leyla', '2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada.'),
+(2, 'img/parrot_2.jpg', 'African Grey', 'Rio', '1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(3, 'img/parrot_3.jpg', 'African Grey', 'Leroy', '3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(4, 'img/parrot_4.jpg', 'African Grey', 'Hanni', '4', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(5, 'img/parrot_5.jpg', 'African Grey', 'Rachel', '5', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(6, 'img/parrot_6.jpg', 'African Grey', 'Levi', '10', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(7, 'img/parrot_7.jpg', 'African Grey', 'Lini', '22', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(8, 'img/parrot_8.jpg', 'African Grey', 'Didi', '38', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(9, 'img/parrot_9.jpg', 'African Grey', 'Henry', '1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(10, 'img/parrot_10.jpg', 'African Grey', 'Lynn', '55', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(11, 'img/parrot_11.jpg', 'African Grey', 'Mietzi', '60', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. '),
+(12, 'img/parrot_12.jpg', 'African Grey', 'Manolo', '57', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Vel turpis nunc eget lorem dolor sed viverra ipsum. Dui ut ornare lectus sit amet est placerat in egestas. Nisl tincidunt eget nullam non nisi est. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Sapien et ligula ullamcorper malesuada. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Mauris ultrices eros in cursus turpis massa. Euismod in pellentesque massa placerat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst. ');
 
 -- --------------------------------------------------------
 
@@ -63,16 +64,17 @@ CREATE TABLE `users` (
   `userId` int(11) NOT NULL,
   `userName` varchar(30) NOT NULL,
   `userEmail` varchar(60) NOT NULL,
-  `userPass` varchar(255) NOT NULL
+  `userPass` varchar(255) NOT NULL,
+  `userStatus` enum('User','Admin') DEFAULT 'User'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userId`, `userName`, `userEmail`, `userPass`) VALUES
-(1, 'User', 'user@email.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'),
-(2, 'admin', 'admin@email.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+INSERT INTO `users` (`userId`, `userName`, `userEmail`, `userPass`, `userStatus`) VALUES
+(1, 'User', 'user@email.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'User'),
+(2, 'admin', 'admin@email.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -99,7 +101,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `birds`
 --
 ALTER TABLE `birds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `users`
